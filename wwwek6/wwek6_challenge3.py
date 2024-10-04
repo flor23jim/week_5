bf15f6dc112089368a1af29789e335cb20aa88a4
Jimenez <FNJimenez@cps.k12.il.us>
2024-10-04 12:46:18 -0500

    callenge3_week6

---
+++ wwwek6/wwek6_challenge3.py
@@ -0,0 +1,20 @@
+text=input("give me a sentence")
+lowercase_text=text.lower()
+one=input("Provide one random letter")
+two=input("Provide one random letter")
+three=input("Provide one random letter")
+print(one)
+print(two)
+print(three)
+print(text.count(one))
+print(text.count(two))
+print(text.count(three))
+
+
+
+print(text.count)
+
+print(len(text))
+
+print(text[0])
+print(text[-1])
