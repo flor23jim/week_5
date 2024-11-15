954b31c72120b7aa6a90dadbc625e51e8de38b4e
Jimenez <FNJimenez@cps.k12.il.us>
2024-11-15 12:32:53 -0600

    worksheet-practice

--- week 10_day3.py
+++ week 10_day3.py
@@ -8,18 +8,18 @@
 # Below 50: Print "Fail"
 # Stop the loop and print the final count of each category when the user enters a negative number.
 
-# question=int(input("please provide me with a set scores all individually:"))# varible is saved as question it allows us to save the users input. In addition it provides us a variable to continuously use. 
-# while not question<0: # if input score is less than zero loop continues till score is an effectivie number
-#     if question>=90: # if score provided is greater than or equal to 90 excellent is printed 
-#          print("excellent")
-#     elif 70<=question<=89: # if score provided is greater than 70 or less than or equal to 89 good is printed
-#          print("good")
-#     elif 50<=question<=69: # if score provided is greater than 50 or less than or equal to 69 pass is printed
-#          print("pass")
-#     elif 0<question<50: # if score provided os greater than 0 or less than 50  fail is printed 
-#          print("Fail")
-#     question=int(input("please provide me with a set scores all individually:")) # allows for question for continue until not valid is read and printed 
-# print("not vaild bye") # if score is invaled meaning greater than 100 or less than 0 not valid bye is printed
+question=int(input("please provide me with a set scores all individually:"))# varible is saved as question it allows us to save the users input. In addition it provides us a variable to continuously use. 
+while not question<0: # if input score is less than zero loop continues till score is an effectivie number
+    if question>=90: # if score provided is greater than or equal to 90 excellent is printed 
+         print("excellent")
+    elif 70<=question<=89: # if score provided is greater than 70 or less than or equal to 89 good is printed
+         print("good")
+    elif 50<=question<=69: # if score provided is greater than 50 or less than or equal to 69 pass is printed
+         print("pass")
+    elif 0<question<50: # if score provided os greater than 0 or less than 50  fail is printed 
+         print("Fail")
+    question=int(input("please provide me with a set scores all individually:")) # allows for question for continue until not valid is read and printed 
+print("not vaild bye") # if score is invaled meaning greater than 100 or less than 0 not valid bye is printed
 #####################################################################################################################################     
               
 # Problem 2:Even and Odd Counter with Conditions
@@ -31,7 +31,7 @@
 # Print the counts for both “special even” and “special odd” numbers.
 
 num1=int(input("please give me a starting number"))
-num2=int(input('please provided me with a ending number'))
+num2=int(input('please provide me with a ending number'))
 
 for x in range(num1,num2+1): 
      if x % 2==0:
