1778ef61d89b4dbf31c2f603768746ab44932b92
Jimenez <FNJimenez@cps.k12.il.us>
2024-12-02 12:06:21 -0600

    week15

---
+++ week15_day1.py
@@ -0,0 +1,58 @@
+# Booleans
+# Check Even or Odd: Write a program to check if a number entered by the user is even or odd.
+# Input: Integer
+# Output: "Even" or "Odd"
+
+# number=int(input("please provide me with a number"))
+# if (number% 2)==0:
+#     print("the numebr is even") 
+# else:
+#     print("the number is odd") 
+
+# Comparison Operators
+# Max of Three Numbers: Compare three numbers entered by the user and print the largest.
+# Input: Three integers
+# Output: Largest number
+
+# integer=int(input("please provide numbers"))
+# integer1=int(input("please provide numbers"))
+# integer2=int(input("please provide numbers"))
+# print(max(integer,integer1,integer2))
+
+# Decision Making
+# Weekend or Weekday: Write a program that checks if a given day is a weekday or a weekend.
+# Input: String ("Monday", "Tuesday", etc.)
+# Output: "Weekday" or "Weekend"
+
+# x=input("please provided me wiht a day")
+# weekday=["monday","tuesday","wednesday","thurday","friday"]
+# if x in weekday:
+#     print("weekday")
+# else:
+#     print("weekend")
+
+# List Comprehension
+# Cubed Numbers: Create a list of cubed values for numbers from 1 to 10 using list comprehension.
+# Output: List of cubes
+
+# value=[1,2,3,4,5,6,7,8,9,10]
+# cubev=[x**3 for x in value]
+# print(cubev)
+
+# Logical Operators
+# Divisible by 2 and 5: Check if a number is divisible by both 2 and 5.
+# Input: Integer
+# Output: "Divisible by 2 and 5" or "Not divisible by 2 and 5"
+
+# num=int(input("give me a number"))
+# if num%2 ==0 and num%5 ==0:
+#     print("divisible by 2 and 5")
+# else: 
+#     print("not divisible by 2 and 5")
+
+# Loops
+# Count Down: Use a while loop to count down from 20 to 0.
+# Output: Print each number
+
+for i in range (21,0):
+    print(i)
